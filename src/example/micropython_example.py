345db23b12ec607fa70e6cdd416d75eb3715201b
Shubham Singh <sb2001nov@gmail.com>
2021-12-08 12:05:57 +0530

    Update micropython_example.py
    formatted

--- src/example/micropython_example.py
+++ src/example/micropython_example.py
@@ -21,27 +21,35 @@ btn9_pin = board.GP16
 btn1 = digitalio.DigitalInOut(btn1_pin)
 btn1.direction = digitalio.Direction.INPUT
 btn1.pull = digitalio.Pull.DOWN
+
 btn2 = digitalio.DigitalInOut(btn2_pin)
 btn2.direction = digitalio.Direction.INPUT
 btn2.pull = digitalio.Pull.DOWN
+
 btn3 = digitalio.DigitalInOut(btn3_pin)
 btn3.direction = digitalio.Direction.INPUT
 btn3.pull = digitalio.Pull.DOWN
+
 btn4 = digitalio.DigitalInOut(btn4_pin)
 btn4.direction = digitalio.Direction.INPUT
 btn4.pull = digitalio.Pull.DOWN
+
 btn5 = digitalio.DigitalInOut(btn5_pin)
 btn5.direction = digitalio.Direction.INPUT
 btn5.pull = digitalio.Pull.DOWN
+
 btn6 = digitalio.DigitalInOut(btn6_pin)
 btn6.direction = digitalio.Direction.INPUT
 btn6.pull = digitalio.Pull.DOWN
+
 btn7 = digitalio.DigitalInOut(btn7_pin)
 btn7.direction = digitalio.Direction.INPUT
 btn7.pull = digitalio.Pull.DOWN
+
 btn8 = digitalio.DigitalInOut(btn8_pin)
 btn8.direction = digitalio.Direction.INPUT
 btn8.pull = digitalio.Pull.DOWN
+
 btn9 = digitalio.DigitalInOut(btn9_pin)
 btn9.direction = digitalio.Direction.INPUT
 btn9.pull = digitalio.Pull.DOWN
@@ -53,29 +61,31 @@ while True:
     if btn1.value:
         keyboard.send(Keycode.ONE)
         time.sleep(0.1)
-    if btn2.value:
+    elif btn2.value:
         keyboard.send(Keycode.FOUR)
         time.sleep(0.1)
-    if btn3.value:
+    elif btn3.value:
         keyboard.send(Keycode.SEVEN)
         time.sleep(0.1)
-    if btn4.value:
+    elif btn4.value:
         keyboard.send(Keycode.TWO)
         time.sleep(0.1)
-    if btn5.value:
+    elif btn5.value:
         keyboard.send(Keycode.FIVE)
         time.sleep(0.1)
-    if btn6.value:
+    elif btn6.value:
         keyboard.send(Keycode.EIGHT)
         time.sleep(0.1)
-    if btn7.value:
+    elif btn7.value:
         keyboard.send(Keycode.THREE)
         time.sleep(0.1)
-    if btn8.value:
+    elif btn8.value:
         keyboard.send(Keycode.SIX)
         time.sleep(0.1)
-    if btn9.value:
+    elif btn9.value:
         keyboard.send(Keycode.NINE)
         time.sleep(0.1)
+    else:
+        pass
 
     time.sleep(0.1)
